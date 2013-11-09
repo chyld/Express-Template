5b3fc9a5ce729649b17516ef07e16f726cae0b08
Chyld Medford <chyld.medford@gmail.com>
2013-11-09 00:49:55 -0600

    added sendAjaxRequest

--- public/javascripts/app/utilities.js
+++ public/javascripts/app/utilities.js
@@ -21,3 +21,21 @@ function parseLowerCase(string){
 function formatCurrency(number){
   return '$' + number.toFixed(2);
 }
+
+function sendAjaxRequest(url, data, verb, altVerb, event, successFn){
+  var options = {};
+  options.url = url;
+  options.type = verb;
+  options.data = data;
+  options.success = successFn;
+  options.error = function(jqXHR, status, error){console.log(error);};
+
+  if(altVerb)
+    if(typeof data === 'string')
+      options.data += '&_method=' + altVerb;
+    else
+      options.data._method = altVerb;
+
+  $.ajax(options);
+  if(event) event.preventDefault();
+}
